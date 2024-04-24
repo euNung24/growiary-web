62c976bac63c912d42b92f3071da28d003c62d9f
eunung <pandamon24@naver.com>
2024-04-24 10:12:46 +0900

    Test: Cookie test

---
+++ src/app/(withAside)/example/page.tsx
@@ -0,0 +1,12 @@
+// import GoogleLoginExample from '@/components/GoogleLoginExample';
+import CookieTest from '@/components/CookieTest';
+
+export default function Home() {
+  return (
+    <>
+      Cookie Example
+      <br />
+      <CookieTest />
+    </>
+  );
+}
