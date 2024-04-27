c47e31be581c5d9f9ae0eea71798c957283dde86
eunung <pandamon24@naver.com>
2024-04-27 15:09:53 +0900

    Feat: 공통 스타일 추가

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -2,12 +2,16 @@ import { cn } from '@/lib/utils';
 import * as React from 'react';
 import { HTMLAttributes } from 'react';
 
-const Chip = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+type ChipProps = HTMLAttributes<HTMLDivElement> & {
+  variant?: 'secondary';
+};
+const Chip = ({ className, variant, ...props }: ChipProps) => {
   return (
     <div
       {...props}
       className={cn(
-        'inline-block font-r14 rounded px-2 py-0.5 self-start bg-primary-400 text-white-0',
+        'inline-block font-r14 rounded px-2 py-0.5 self-start',
+        variant ? 'bg-secondary-50 text-gray-900' : 'bg-primary-400 text-white-0',
         className,
       )}
     />
