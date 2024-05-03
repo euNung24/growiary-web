cdcf6648cf2578819e2c26628c2b4a19e06fe87e
eunung <pandamon24@naver.com>
2024-05-03 20:51:45 +0900

    Feat: 매일 글쓰기 - 미로그인 분기 추가

--- src/views/home/HomeDailyChecker.tsx
+++ src/views/home/HomeDailyChecker.tsx
@@ -6,6 +6,8 @@ import { getDailyCheckerPost } from '@/apis/post';
 import { useRecoilValue } from 'recoil';
 import { TodayState } from '@/store/todayStore';
 import Link from 'next/link';
+import { getCookie } from '@/utils';
+import { useMutation } from '@tanstack/react-query';
 
 const HomeDailyChecker = () => {
   const {
@@ -15,61 +17,79 @@ const HomeDailyChecker = () => {
   const headerDescriptionStyle = 'font-r16 text-gray-700 mt-1 mb-6';
   const [data, setData] = useState<number[] | undefined>(undefined);
   const today = new Date();
+  const mutation = useMutation({
+    mutationKey: ['dailyChecker'],
+    mutationFn: getDailyCheckerPost,
+  });
 
   useEffect(() => {
-    const fn = async () => {
-      const res = await getDailyCheckerPost();
-      setData(res.data.post);
-    };
+    if (!getCookie('accessToken')) return;
 
-    fn();
+    mutation.mutateAsync('').then(res => {
+      setData(res.data.post);
+    });
   }, []);
 
   return (
     <section>
-      <div className="flex justify-between">
-        <h2 className="title">매일 글쓰기</h2>
-      </div>
-      <p className={headerDescriptionStyle}>
-        오늘까지 연속으로{' '}
-        <span className="font-sb16 text-primary-900">{data || 0}일</span> 기록에
-        성공했어요
-      </p>
-      <div className="border border-gray-200 rounded-xl flex justify-center gap-x-[52px] py-10 overflow-hidden">
-        {[...Array(day)].map((v, i) => (
-          <DailyChecker
-            key={i}
-            variant="prev"
-            date={new Date(
-              today.getFullYear(),
-              today.getMonth(),
-              today.getDate() - 1,
-              0,
-              0,
-              0,
-            ).getDate()}
-            count={i !== 0 && data ? data[i - 1] : 0}
-          />
-        ))}
-        <Link href="/post">
-          <DailyChecker variant="today" date={today.getDate()} count={data?.[day - 1]} />
-        </Link>
-        {[...Array(8 - day)].map((v, i) => (
-          <DailyChecker
-            key={i}
-            variant="next"
-            date={new Date(
-              today.getFullYear(),
-              today.getMonth(),
-              today.getDate() + i + 1,
-              0,
-              0,
-              0,
-            ).getDate()}
-            count={data && i + day !== data.length ? data[i + day] : 0}
-          />
-        ))}
-      </div>
+      {data ? (
+        <>
+          <div className="flex justify-between">
+            <h2 className="title">매일 글쓰기</h2>
+          </div>
+          <p className={headerDescriptionStyle}>
+            오늘까지 연속으로{' '}
+            <span className="font-sb16 text-primary-900">{data || 0}일</span> 기록에
+            성공했어요
+          </p>
+          <div className="border border-gray-200 rounded-xl flex justify-center gap-x-[52px] py-10 overflow-hidden">
+            {[...Array(day)].map((v, i) => (
+              <DailyChecker
+                key={i}
+                variant="prev"
+                date={new Date(
+                  today.getFullYear(),
+                  today.getMonth(),
+                  today.getDate() - 1,
+                  0,
+                  0,
+                  0,
+                ).getDate()}
+                count={i !== 0 && data ? data[i - 1] : 0}
+              />
+            ))}
+            <Link href="/post">
+              <DailyChecker
+                variant="today"
+                date={today.getDate()}
+                count={data?.[day - 1]}
+              />
+            </Link>
+            {[...Array(8 - day)].map((v, i) => (
+              <DailyChecker
+                key={i}
+                variant="next"
+                date={new Date(
+                  today.getFullYear(),
+                  today.getMonth(),
+                  today.getDate() + i + 1,
+                  0,
+                  0,
+                  0,
+                ).getDate()}
+                count={data && i + day !== data.length ? data[i + day] : 0}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex justify-between">
+            <h2 className="title">그루어리는 어떤 서비스인가요?</h2>
+          </div>
+          <div className="mt-6 border border-gray-200 rounded-xl flex justify-center gap-x-[52px] py-10 overflow-hidden"></div>
+        </>
+      )}
     </section>
   );
 };
