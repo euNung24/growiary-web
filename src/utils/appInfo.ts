1ed39fd68b8af1a56af08ed82708bb1b4d0809ad
eunung <pandamon24@naver.com>
2024-05-21 13:48:32 +0900

    Feat: pwa 설정

---
+++ src/utils/appInfo.ts
@@ -0,0 +1,8 @@
+export const APP_INFO = {
+  NAME: '그루어리',
+  DEFAULT_TITLE: '그루어리 : 기록하고 성장하는 어른들을 위한 노트 서비스',
+  TITLE_TEMPLATE: '%s - 그루어리',
+  DESCRIPTION:
+    '나만의 생각과 경험, 일상을 기록하고 데이터 리포트로 확인해보세요. 나도 몰랐던 나의 기록 패턴, 성향, 지난 날의 여정과 생각들을 돌아볼 수 있을 거예요.',
+  URL: new URL('https://growiary.com'),
+};
