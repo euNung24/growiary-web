3cb9d8fe6908acb500dd4bde6e2550a99f85931f
eunung <pandamon24@naver.com>
2024-06-07 18:25:19 +0900

    Feat: 함수 잘못된 네이밍 수정

--- src/apis/post.ts
+++ src/apis/post.ts
@@ -4,43 +4,43 @@ import {
   ResPostType,
   UpdatePostType,
 } from '@/types/postTypes';
-import withTokenPost from '@/apis/withToken';
+import withToken from '@/apis/withToken';
 import { TopicCategory } from '@/types/topicTypes';
 import { ApiSuccessResponse } from '@/types';
 
 const postApiUrl = process.env.NEXT_PUBLIC_API + '/post';
 
 export const getAllPosts = () =>
-  withTokenPost(postApiUrl + '/all') as Promise<ApiSuccessResponse<ResPostType[]>>;
+  withToken(postApiUrl + '/all') as Promise<ApiSuccessResponse<ResPostType[]>>;
 
 export const findPost = (id: string) =>
-  withTokenPost(postApiUrl + '/find', { body: { id } }) as Promise<
+  withToken(postApiUrl + '/find', { body: { id } }) as Promise<
     ApiSuccessResponse<ResPostType>
   >;
 
 export const createPost = (postData: ReqPostType) =>
-  withTokenPost(postApiUrl + '/create', { body: postData }) as Promise<
+  withToken(postApiUrl + '/create', { body: postData }) as Promise<
     ApiSuccessResponse<ResPostType[]>
   >;
 
 export const updatePost = (postData: UpdatePostType) =>
-  withTokenPost(postApiUrl + '/update', { body: postData }) as Promise<
+  withToken(postApiUrl + '/update', { body: postData }) as Promise<
     ApiSuccessResponse<ResPostType>
   >;
 
 export const deletePost = (id: string) =>
-  withTokenPost(postApiUrl + '/update', { body: { status: false, id } }) as Promise<
+  withToken(postApiUrl + '/update', { body: { status: false, id } }) as Promise<
     ApiSuccessResponse<ResPostType>
   >;
 export const getMonthlyPosts = (
   date: string,
   { abortController }: { abortController: AbortController | null },
 ) =>
-  withTokenPost(postApiUrl + '/record', { body: { date }, abortController }) as Promise<
+  withToken(postApiUrl + '/record', { body: { date }, abortController }) as Promise<
     ApiSuccessResponse<{ posts: ResPostType[]; category: Record<TopicCategory, number> }>
   >;
 
 export const getDailyCheckerPost = () =>
-  withTokenPost(postApiUrl + '/continue-range') as Promise<
+  withToken(postApiUrl + '/continue-range') as Promise<
     ApiSuccessResponse<DailyCheckerType>
   >;
