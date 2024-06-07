e8cf5146006c2e1a9297b894666359e4c4303cac
eunung <pandamon24@naver.com>
2024-06-07 19:42:16 +0900

    Feat: 기록 데이터 보기 abortController 적용

--- src/apis/report.ts
+++ src/apis/report.ts
@@ -4,5 +4,10 @@ import { ApiSuccessResponse } from '@/types';
 
 const reportApiUrl = process.env.NEXT_PUBLIC_API + '/report';
 
-export const getReport = (date: string) =>
-  withToken(reportApiUrl, { body: { date } }) as Promise<ApiSuccessResponse<ReportType>>;
+export const getReport = (
+  date: string,
+  { abortController }: { abortController: AbortController | null },
+) =>
+  withToken(reportApiUrl, { body: { date }, abortController }) as Promise<
+    ApiSuccessResponse<ReportType>
+  >;
--- src/hooks/report/useGetReport.tsx
+++ src/hooks/report/useGetReport.tsx
@@ -3,11 +3,24 @@
 import { getReport } from '@/apis/report';
 import { getNewAccessToken } from '@/utils/api';
 import { useMutation } from '@tanstack/react-query';
+import { useRef } from 'react';
 
 const useGetReport = () => {
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  const checkAbort = (date: string) => {
+    if (abortControllerRef.current) {
+      abortControllerRef.current.abort();
+    }
+
+    // 새로운 AbortController를 생성합니다.
+    abortControllerRef.current = new AbortController();
+    return getReport(date, { abortController: abortControllerRef.current });
+  };
+
   return useMutation({
     mutationKey: ['report'],
-    mutationFn: (date: string) => getReport(date),
+    mutationFn: (date: string) => checkAbort(date),
     onError: async error => {
       await getNewAccessToken(error);
       console.log(error.message);
