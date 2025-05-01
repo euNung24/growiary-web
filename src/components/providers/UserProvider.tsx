8953794dd1bb0dd08f93ac6fa4da5d55edd748b7
eunung <eunung24@gmail.com>
2025-05-01 21:43:57 +0900

    fix: performance measure 예외 처리 추가

--- src/components/providers/UserProvider.tsx
+++ src/components/providers/UserProvider.tsx
@@ -8,8 +8,14 @@ const UserProvider = ({ children }: PropsWithChildren) => {
   const { data } = useGetProfile();
 
   useEffect(() => {
-    performance.mark('landing-to-main-end');
-    performance.measure('LandingToMain', 'landing-to-main-start', 'landing-to-main-end');
+    if (performance.getEntriesByName('landing-to-main-start').length > 0) {
+      performance.mark('landing-to-main-end');
+      performance.measure(
+        'LandingToMain',
+        'landing-to-main-start',
+        'landing-to-main-end',
+      );
+    }
   }, []);
 
   return <div className={cn('h-full', !data ? 'mt-0' : 'pt-[72px]')}>{children}</div>;
