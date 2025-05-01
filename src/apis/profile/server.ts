1a1f040401c656cd56d014662f7a30829e7a37fe
eunung <eunung24@gmail.com>
2025-05-01 20:21:39 +0900

    refactor: 서버 api 요청 추상화

--- src/apis/profile/server.ts
+++ src/apis/profile/server.ts
@@ -1,43 +1,15 @@
 import { handleError } from '@/apis/token/server';
 import { ProfileType } from '@/types/profileTypes';
-import { cookies } from 'next/headers';
+import { fetchOnServer } from '@/utils/fetchOnServer';
 
 const profileApiUrl = process.env.NEXT_PUBLIC_API + '/profile';
 
 export const getProfile = async (): Promise<ProfileType | undefined> => {
-  const request = async (token?: string) => {
-    const accessToken = token || cookies().get('accessToken')?.value;
-
-    if (!accessToken) return null;
-
-    const response = await fetch(profileApiUrl, {
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    if (!response.ok) {
-      if (response.status === 400) {
-        let message = 'Unknown error';
-        try {
-          const res = await response.json();
-          message = res.message;
-        } catch (e) {
-          if (e instanceof Error) {
-            message = e.message;
-          }
-        }
-        throw new Error(message);
-      }
-    }
-
-    return await response.json();
-  };
+  const profileApi = fetchOnServer(profileApiUrl);
 
   try {
-    return await request();
+    return await profileApi();
   } catch (error) {
-    await handleError(error, request);
+    await handleError(error, profileApi);
   }
 };
