28c5f3beda38881e7e3dfb0295fbfd58070f6d29
eunung <eunung24@gmail.com>
2025-05-02 17:48:13 +0900

    fix: 경로 오류 처리

--- src/app/(withAside)/history/[id]/edit/page.tsx
+++ src/app/(withAside)/history/[id]/edit/page.tsx
@@ -1,9 +1,10 @@
-import PostView from '@/views/PostView';
+import PostView from '@/views/post/PostView';
 import { Suspense } from 'react';
 
 type PageProps = {
   params: { id: string };
 };
+
 export default async function Home({ params: { id } }: PageProps) {
   return (
     <Suspense>
