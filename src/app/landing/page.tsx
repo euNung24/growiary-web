e2f6d29278c9b2ad8c09d2c5eb770b80972d5038
eunung <eunung24@gmail.com>
2025-05-02 15:36:12 +0900

    refactor: pages 불필요한 async 제거

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -2,7 +2,7 @@ import LandingInitEffects from '@/views/landing/LandingInitEffects';
 import StartButton from '@/views/landing/StartButton';
 import LandingView from '@/views/LandingView';
 
-export default async function Home() {
+export default function Home() {
   return (
     <>
       <LandingView>
