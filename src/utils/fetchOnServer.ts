350ced21588489dea79787448746b18df2376687
eunung <eunung24@gmail.com>
2025-05-02 17:08:42 +0900

    refactor: 서버 fetch 공통 함수 에러 핸들링 추상화

--- src/utils/fetchOnServer.ts
+++ src/utils/fetchOnServer.ts
@@ -1,32 +1,41 @@
+import { handleError } from '@/apis/token/server';
 import { cookies } from 'next/headers';
 
-export const fetchOnServer = (api: string, config?: RequestInit) => async (token?: string) => {
-  const accessToken = token || cookies().get('accessToken')?.value;
+const createServerFetcher =
+  (api: string, config?: RequestInit) => async (token?: string) => {
+    const accessToken = token || cookies().get('accessToken')?.value;
 
-  if (!accessToken) return null;
+    if (!accessToken) return null;
 
-  const response = await fetch(api, {
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      Authorization: `Bearer ${accessToken}`,
-    },
-    ...config,
-  });
+    try {
+      const response = await fetch(api, {
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        ...config,
+      });
 
-  if (!response.ok) {
-    if (response.status === 400) {
-      let message = 'Unknown error';
-      try {
-        const res = await response.json();
-        message = res.message;
-      } catch (e) {
-        if (e instanceof Error) {
-          message = e.message;
+      if (!response.ok) {
+        if (response.status === 400) {
+          let message = 'Unknown error';
+          try {
+            const res = await response.json();
+            message = res.message;
+          } catch (e) {
+            if (e instanceof Error) {
+              message = e.message;
+            }
+          }
+          throw new Error(message);
         }
       }
-      throw new Error(message);
-    }
-  }
 
-  return await response.json();
-};
+      return await response.json();
+    } catch (error) {
+      return await handleError(error, createServerFetcher(api, config));
+    }
+  };
+
+export const fetchOnServer = (api: string, config?: RequestInit) =>
+  createServerFetcher(api, config)();
