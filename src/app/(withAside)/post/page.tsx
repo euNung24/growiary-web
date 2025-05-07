af6db36c6a5fdb2dc697dd381a50284da09470a9
euNung24 <eunung24@gmail.com>
2025-05-07 21:49:01 +0900

    [Refactor]: 기록하기 form 내부 의존성 개선 및 일부 공통 함수 분리  (#7)
    * refactor: PostPage 컴포넌트 내부 분리 및 관련 동작 일부 수정
    
    * fix: 경로 오류 처리
    
    * refactor: editor suspense로 감싸기
    
    * chore: 불필요한 console 제거
    
    * refactor: ReactQuill 초기 placeholder  조건문 개선
    
    * chore: console.log 제거

--- src/app/(withAside)/post/page.tsx
+++ src/app/(withAside)/post/page.tsx
@@ -1,6 +1,7 @@
-import PostView from '@/views/PostView';
 import { Suspense } from 'react';
 import Preview from '@/views/common/Preview';
+import PostView from '@/views/post/PostView';
+
 export default function Home() {
   return (
     <Suspense>
