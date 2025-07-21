a36b16abed8a94b1bdb595b1c6248299710853c9
eunung <eunung24@gmail.com>
2025-07-21 18:46:40 +0900

    refactor: ReportView 불필요한 코드 제거

--- src/user/features/report/components/ReportView.tsx
+++ src/user/features/report/components/ReportView.tsx
@@ -10,37 +10,24 @@ import withMoveMonthly from '@/user/features/report/components/withMoveMonthly';
 import ReportTotal from '@/user/features/report/components/ReportTotal';
 import useReportContext from '@/user/features/report/hooks/useReportContext';
 import useProfileContext from '@/shared/hooks/useProfileContext';
-// import AdvanceReservation from '@/views/report/AdvanceReservation';
-// import { useRecoilValue } from 'recoil';
 
 const ReportView = () => {
-  const { data } = useReportContext();
-  const { isLogin } = useProfileContext();
-  // const {
-  //   date: { year, month },
-  // } = useRecoilValue(TodayState);
-
   return (
     <article className="flex flex-col w-full gap-y-[102px] mt-6">
-      {((isLogin === 'LOGIN' && data) || isLogin === 'NOT_LOGIN') && (
-        <>
-          <ReportTotal />
-          <ReportPost />
-          <section>
-            <h2 className="title">기록 패턴</h2>
-            <div className="flex gap-5 flex-wrap [&>*]:flex-1 sm:[&>*]:flex-initial">
-              <ReportByDay />
-              <ReportByTime />
-            </div>
-          </section>
-          <ReportByChar />
-          <ReportByTopic />
-          <ReportByTag />
-        </>
-      )}
-      {/*{isLogin === 'LOGIN' && year === selectedYear && month === selectedMonth && (*/}
-      {/*  <AdvanceReservation />*/}
-      {/*)}*/}
+      <>
+        <ReportTotal />
+        <ReportPost />
+        <section>
+          <h2 className="title">기록 패턴</h2>
+          <div className="flex gap-5 flex-wrap [&>*]:flex-1 sm:[&>*]:flex-initial">
+            <ReportByDay />
+            <ReportByTime />
+          </div>
+        </section>
+        <ReportByChar />
+        <ReportByTopic />
+        <ReportByTag />
+      </>
     </article>
   );
 };
